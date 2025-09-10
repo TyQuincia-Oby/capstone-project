e508cb5243133b5738420813940c65731710a0ac
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-09-10 20:43:13 +0000

    chore: added onEvents for all potions and spells, started adding cards for spells, ids for all elements added, adding show/hide features for potion and spell buttons, lists and cards

--- app.js
+++ app.js
@@ -5,15 +5,85 @@ console.log(document.getElementById("generateBtn"));
 
 onEvent("generateBtn", "click", function() {
     console.log("Generate button clicked");
+    
     setText("output-display", "You generated a question!");
 });
 
+
+//All things Spells
 onEvent("spellBtn", "click", function() {
     console.log("Spell button clicked");
-        
+    //show list of spells
+    document.getElementById("spellList").classList.remove("d-none");
+    document.getElementById("spell-display").classList.add("d-none");
+    console.log("List of Spells Shown in spell-display")
 });
 
+//Lumos
+onEvent("lu", "click", function(){
+    console.log("Lumos Spell Clicked");
+    //show Lumos Spell card
+
+});
+
+//Disarming Charm
+onEvent("dc", "click", function(){
+    console.log("Disarming Charm Clicked")
+    //show Disarming Charm Spell Card
+
+
+});
+
+//Patronus Charm
+onEvent("lc", "click", function(){
+    console.log("Patronus Charm Clicked")
+    //show Patronus Charm Spell Card
+
+});
+
+//Boggart Banishing Spell
+onEvent("bbs", "click", function(){
+    console.log("Boggart Banishing Spell Clicked")
+    //show Boggart Banishing Spell Card
+
+});
+
+
+
+
+//All things Potions
 onEvent("potionBtn", "click", function() {
     console.log("Potion button clicked");
-    
+    //show list of potions
+    document.getElementById("potionList").classList.remove("d-none");
+    document.getElementById("potion-display").classList.add("d-none");
+    console.log("List of Potions Shown in potion-display")
+});
+
+//Polyjuice Potion
+onEvent("pp", "click", function() {
+    console.log("Polyjuice Potion Clicked");
+    //show Polyjuice Potion card
+
+});
+
+//Beautification Potion
+onEvent("bp", "click", function(){
+    console.log("Beautification Potion Clicked");
+    //show Beautification Potion card
+
+});
+
+//Elixir of Life Potion
+onEvent("eol", "click", function(){
+    console.log("Elixir of Life Potion Clicked");
+    //show Elixir of Life Potion card
+
+});
+
+//Amortentia Potion
+onEvent("am", "click", function(){
+    console.log("Amortentia Potion Clicked");
+    //show Amortentia Potion card
+
 });
