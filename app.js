b0cc248bceeb23387a146d1107e7b7d0dd90bbf5
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-09-10 17:46:16 +0000

    style: added cards for potions and spells with attributes and added comments for navigating html page

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("Hello from app.js");
+setProperty("spellBtn", "border", "3px solid black");
+setProperty("potionBtn", "border", "3px solid black");
+console.log(document.getElementById("generateBtn"));
+
+onEvent("generateBtn", "click", function() {
+    console.log("Generate button clicked");
+    setText("output-display", "You generated a question!");
+});
+
+onEvent("spellBtn", "click", function() {
+    console.log("Spell button clicked");
+        
+});
+
+onEvent("potionBtn", "click", function() {
+    console.log("Potion button clicked");
+    
+});
